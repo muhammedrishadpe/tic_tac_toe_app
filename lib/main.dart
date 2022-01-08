9fa70ca9cda85855f4df7b6af3a28c01ace5970e
muhammedrishadpe <rishadpe@gmail.com>
2022-01-08 12:57:51 +0530

    Tapped index

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tic_tac_toe/board_tile.dart';
+import 'package:tic_tac_toe/tile_state.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +12,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var _boardState = List.filled(9, TileState.EMPTY);
+  // [[123] [456] [789]]
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,54 +33,29 @@ class _MyAppState extends State<MyApp> {
   Widget _boardTiles() {
     return Builder(builder: (context) {
       final boardDimension = MediaQuery.of(context).size.width;
-      final titleDimension = boardDimension / 3;
+      final tileDimension = boardDimension / 3;
       return Container(
-        width: boardDimension,
-        height: boardDimension,
-        child: Column(
-          children: [
-            Row(
-              children: [
-                BoardTile(
-                  dimension: titleDimension,
-                ),
-                BoardTile(
-                  dimension: titleDimension,
-                ),
-                BoardTile(
-                  dimension: titleDimension,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                BoardTile(
-                  dimension: titleDimension,
-                ),
-                BoardTile(
-                  dimension: titleDimension,
-                ),
-                BoardTile(
-                  dimension: titleDimension,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                BoardTile(
-                  dimension: titleDimension,
-                ),
-                BoardTile(
-                  dimension: titleDimension,
-                ),
-                BoardTile(
-                  dimension: titleDimension,
-                ),
-              ],
-            )
-          ],
-        ),
-      );
+          width: boardDimension,
+          height: boardDimension,
+          child: Column(
+              children: chunk(_boardState, 3).asMap().entries.map((entry) {
+            final chunkIndex = entry.key;
+            final tileStateChunk = entry.value;
+
+            return Row(
+              children: tileStateChunk.asMap().entries.map((innerEntry) {
+                final innerIndex = innerEntry.key;
+                final tileState = innerEntry.value;
+                final tileIndex = (chunkIndex * 3) + innerIndex;
+
+                return BoardTile(
+                  tileState: tileState,
+                  dimension: tileDimension,
+                  onPressed: () => print('Tapped index $tileIndex'),
+                );
+              }).toList(),
+            );
+          }).toList()));
     });
   }
 }
